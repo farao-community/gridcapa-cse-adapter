b0ea583f4afa34821a7fb2db4d53a6e735416ce3
Joris Mancini <joris.mancini@rte-france.com>
2022-05-10 11:45:46 +0200

    Unused import

--- cse-adapter-app/src/test/java/com/farao_community/farao/cse/adapter/app/CseAdapterListenerTest.java
+++ cse-adapter-app/src/test/java/com/farao_community/farao/cse/adapter/app/CseAdapterListenerTest.java
@@ -18,7 +18,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.ThrowingSupplier;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
