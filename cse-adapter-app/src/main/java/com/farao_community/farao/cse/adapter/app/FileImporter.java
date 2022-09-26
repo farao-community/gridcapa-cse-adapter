d7bcd27287ace430be3b0b2e962e357586d14404
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2022-09-26 17:19:51 +0200

    create minio-link-expiry event for user interface

--- cse-adapter-app/src/main/java/com/farao_community/farao/cse/adapter/app/FileImporter.java
+++ cse-adapter-app/src/main/java/com/farao_community/farao/cse/adapter/app/FileImporter.java
@@ -11,6 +11,7 @@ import com.farao_community.farao.cse.adapter.app.model.UserConfiguration;
 import com.farao_community.farao.cse.runner.api.exception.CseInvalidDataException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -21,35 +22,51 @@ import java.net.URL;
 @Service
 public class FileImporter {
 
-    private final UrlValidationService urlValidationService;
+    private final UrlWhitelistConfiguration urlWhitelistConfiguration;
+    private final Logger businessLogger;
 
-    public FileImporter(UrlValidationService urlValidationService) {
-        this.urlValidationService = urlValidationService;
+    public FileImporter(UrlWhitelistConfiguration urlWhitelistConfiguration, Logger businessLogger) {
+        this.urlWhitelistConfiguration = urlWhitelistConfiguration;
+        this.businessLogger = businessLogger;
     }
 
     public UserConfiguration importUserConfiguration(String userConfigurationUrl) {
-        try (InputStream is = urlValidationService.openUrlStream(userConfigurationUrl)) {
+        try (InputStream is = openUrlStream(userConfigurationUrl)) {
             ObjectMapper objectMapper = new ObjectMapper();
             return objectMapper.readValue(is.readAllBytes(), UserConfiguration.class);
         } catch (IOException e) {
-            throw new CseInvalidDataException(String.format("Cannot import user configuration file: %s, check file format: %s", getFilenameFromUrl(userConfigurationUrl), e.getMessage()), e);
+            throw new CseInvalidDataException(String.format("Cannot import user configuration file: %s, check file format: %s", getFileNameFromUrl(userConfigurationUrl), e.getMessage()), e);
         }
     }
 
     public AutomatedForcedPras importAutomatedForcedPras(String automatedForcedPrasUrl) {
-        try (InputStream is = urlValidationService.openUrlStream(automatedForcedPrasUrl)) {
+        try (InputStream is = openUrlStream(automatedForcedPrasUrl)) {
             ObjectMapper objectMapper = new ObjectMapper();
             return objectMapper.readValue(is.readAllBytes(), AutomatedForcedPras.class);
         } catch (IOException e) {
-            throw new CseInvalidDataException(String.format("Cannot import automated forced PRAs file: %s, check file format: %s", getFilenameFromUrl(automatedForcedPrasUrl), e.getMessage()), e);
+            throw new CseInvalidDataException(String.format("Cannot import automated forced PRAs file: %s, check file format: %s", getFileNameFromUrl(automatedForcedPrasUrl), e.getMessage()), e);
         }
     }
 
-    public static String getFilenameFromUrl(String url) {
+    public static String getFileNameFromUrl(String url) {
         try {
             return FilenameUtils.getName(new URL(url).getPath());
         } catch (MalformedURLException e) {
             throw new CseInvalidDataException(String.format("URL is invalid: %s", url));
         }
     }
+
+    public InputStream openUrlStream(String urlString) {
+        try {
+            if (urlWhitelistConfiguration.getWhitelist().stream().noneMatch(urlString::startsWith)) {
+                throw new CseInvalidDataException(String.format("URL '%s' is not part of application's whitelisted url's.", urlString));
+            }
+            URL url = new URL(urlString);
+            return url.openStream(); // NOSONAR Usage of whitelist not triggered by Sonar quality assessment, even if listed as a solution to the vulnerability
+        } catch (IOException e) {
+            businessLogger.error("Error while retrieving content of file : {}, Link may have expired.", getFileNameFromUrl(urlString));
+            throw new CseInvalidDataException(String.format("Exception occurred while retrieving file content from : %s Cause: %s ", urlString, e.getMessage()));
+        }
+    }
+
 }
