096211a78f0fdb0338da7c000f6dc315ff01f530
SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>
2024-08-29 13:43:34 +0200

    Update dependency versions

--- cse-adapter-app/src/main/java/com/farao_community/farao/cse/adapter/app/CseAdapter.java
+++ cse-adapter-app/src/main/java/com/farao_community/farao/cse/adapter/app/CseAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -8,6 +8,8 @@ package com.farao_community.farao.cse.adapter.app;
 
 import com.farao_community.farao.gridcapa.task_manager.api.ProcessRunDto;
 import com.farao_community.farao.gridcapa.task_manager.api.TaskDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -16,11 +18,13 @@ import java.util.List;
  */
 public interface CseAdapter {
 
+    Logger LOGGER = LoggerFactory.getLogger(CseAdapter.class);
     void runAsync(TaskDto taskDto);
 
     static String getCurrentRunId(TaskDto taskDto) {
         List<ProcessRunDto> runHistory = taskDto.getRunHistory();
         if (runHistory == null || runHistory.isEmpty()) {
+            LOGGER.warn("Failed to handle manual run request on timestamp {} because it has no run history", taskDto.getTimestamp());
             throw new CseAdapterException("Failed to handle manual run request on timestamp because it has no run history");
         }
         if (runHistory.size() > 1) {
